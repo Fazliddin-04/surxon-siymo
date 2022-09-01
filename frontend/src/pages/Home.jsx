3d79c72485e330f07bb1a85d678ebc3c1818f8a3
Fazliddin-04 <fazliddinpardayev004@gmail.com>
2022-09-01 17:51:29 +0500

    home page section one created

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,69 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa'
+import { FaQuestionCircle, FaTicketAlt, FaRegCalendarAlt } from 'react-icons/fa'
 
 function Home() {
+  const [date, setDate] = useState(
+    new Date().toLocaleString('en-US', {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+    })
+  )
+
+  const onMutate = (e) => {
+    setDate(
+      new Date(e.target.value).toLocaleString('en-US', {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+      })
+    )
+  }
+
   return (
     <>
-      <section className="heading">
-        <h1>What do you need help with?</h1>
-        <p>Please choose from an option below</p>
+      <section className="home">
+        <div className="flexbox">
+          <div className="form-group">
+            <label htmlFor="company">Company</label>
+            <select name="company" id="company">
+              <option value="Shurchi restoran">Shurchi restoran</option>
+              <option value="filial 1">filial 1</option>
+              <option value="filial 2">filial 2</option>
+            </select>
+          </div>
+          <label htmlFor="date" className="btn btn-outline">
+            {date} <FaRegCalendarAlt />
+            <input
+              type="date"
+              name="date"
+              id="date"
+              onChange={onMutate}
+              defaultValue={new Date().toLocaleDateString('sv-SE')}
+              required
+              style={{ visibility: 'hidden', width: 0 }}
+            />
+          </label>
+        </div>
+        <div className="flexbox">
+          <div className="leftBordered">
+            <small>Total cost</small>
+            <p>3 680 000 $</p>
+          </div>
+          <div className="leftBordered">
+            <small>Total selling price</small>
+            <p>18 680 465 $</p>
+          </div>
+          <div className="leftBordered">
+            <small>Total margin</small>
+            <p>38%</p>
+          </div>
+          <div className="leftBordered">
+            <small>Best selling product</small>
+            <p>iPhone 12 Pro Max</p>
+          </div>
+        </div>
       </section>
 
       <Link to="/tickets" className="btn btn-block">

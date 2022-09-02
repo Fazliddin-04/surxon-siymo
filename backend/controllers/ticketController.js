4e16c6632752479bfc5c4f781b6586bfd594a957
Fazliddin-04 <fazliddinpardayev004@gmail.com>
2022-09-02 11:06:33 +0500

    Ticket Backend Functionality Completed

---
+++ backend/controllers/ticketController.js
@@ -0,0 +1,141 @@
+const asyncHandler = require('express-async-handler')
+
+const User = require('../models/userModel')
+const Ticket = require('../models/ticketModel')
+const e = require('express')
+
+// @desc    Get user tickets
+// @route   /api/tickets
+// @access  PRIVATE
+const getTickets = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id })
+
+  res.status(200).json(tickets)
+})
+
+// @desc    Get user ticket
+// @route   /api/tickets/:id
+// @access  PRIVATE
+const getTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  res.status(200).json(ticket)
+})
+
+// @desc    Create new ticket
+// @route   POST /api/tickets
+// @access  PRIVATE
+const createTicket = asyncHandler(async (req, res) => {
+  const { product, description, price, size, amount } = req.body
+
+  if (!product || !description || !price || !size || !amount) {
+    res.status(400)
+    throw new Error('Please fill out all fields of products')
+  }
+
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.create({
+    user: req.user.id,
+    product, description, price, size, amount,
+    status: 'new'
+  })
+
+  res.status(201).json(ticket)
+})
+
+// @desc    Delete user ticket
+// @route   DELETE /api/tickets/:id
+// @access  PRIVATE
+const deleteTicket = asyncHandler(async (req, res) => {
+  // Delete user using the id in the JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  await ticket.remove()
+
+  res.status(200).json({ success: true })
+})
+
+// @desc    Update ticket
+// @route   PUT /api/tickets/:id
+// @access  PRIVATE
+const updateTicket = asyncHandler(async (req, res) => {
+  // Delete user using the id in the JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+  res.status(200).json(updatedTicket)
+})
+
+module.exports = {
+  getTickets,
+  getTicket,
+  createTicket,
+  updateTicket, deleteTicket
+}
---
+++ backend/models/ticketModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const ticketSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  product: {
+    type: String,
+    required: [true, 'Please select a product'],
+    enum: ['shirt', 'trousers', 'shoes']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please enter a description'],
+  },
+  price: {
+    type: String,
+    required: [true, 'Please enter price of the product'],
+  },
+  size: {
+    type: String,
+    required: [true, 'Please select size of the product'],
+    enum: ['L', 'XL', 'XXL']
+  },
+  amount: {
+    type: Number,
+    required: [true, 'Please enter amount of the product'],
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ['new', 'in stock', 'out of stock'],
+    default: 'new'
+  }
+},
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('Ticket', ticketSchema)
